18f20868048ced689e9afb0d2b7036d7585d4764
Primas <136030538+Primas-0@users.noreply.github.com>
2024-02-04 21:52:24 -0500

    clear and fill done

--- puzzle.cpp
+++ puzzle.cpp
@@ -2,17 +2,18 @@
 #include "puzzle.h"
 Puzzle::Puzzle(int rows, int cols){
     if (rows >= 10 && cols >= 10) {
-        //if the requirements are met, initializes member variables
+        //if the requirements are met, initialize member variables with passed-in values
         m_numRows = rows;
         m_numCols = cols;
 
-        //allocates memory for 2D array
-        m_puzzle = new char*[m_numRows];
+        //allocate memory for 2D array
+        m_puzzle = new char*[m_numRows]; //array of pointers
         for (int i = 0; i < m_numRows; i++) {
+            //each pointer points to a separate array of chars
             m_puzzle[i] = new char[m_numCols];
         }
     } else {
-        //if the requirements are not met, creates an empty object
+        //if the requirements are not met, create an empty object
         m_numRows = 0;
         m_numCols = 0;
         m_puzzle = nullptr;
@@ -20,15 +21,26 @@ Puzzle::Puzzle(int rows, int cols){
 }
 
 Puzzle::~Puzzle(){
-    //deallocates 2D array (must be in reverse order of allocation)
+    //deallocate 2D array (should be done in inside-out order)
     for (int i = 0; i < m_numRows; i++) {
+        //deallocate each array of chars
         delete[] m_puzzle[i];
     }
+    //deallocate the array of pointers
     delete[] m_puzzle;
 }
 
 void Puzzle::clear(){
+    //deallocate all memory
+    for (int i = 0; i < m_numRows; i++) {
+        delete[] m_puzzle[i];
+    }
+    delete[] m_puzzle;
 
+    //clear the current object to an empty object
+    m_numRows = 0;
+    m_numCols = 0;
+    m_puzzle = nullptr;
 }
 
 bool Puzzle::reCreate(int rows, int cols, int seed){
@@ -36,7 +48,19 @@ bool Puzzle::reCreate(int rows, int cols, int seed){
 }
 
 void Puzzle::fill(int seed){
+    int randCharIndex = 0; //where random number will be stored
 
+    //create instance of Random class and set seed
+    Random randObject(MIN, MAX, UNIFORMINT);
+    randObject.setSeed(seed);
+
+    //fill 2D array with random characters
+    for (int i = 0; i < m_numRows; i++) {
+        for (int j = 0; j < m_numCols; j++) {
+            randCharIndex = randObject.getRandNum();
+            m_puzzle[i][j] = ALPHA[randCharIndex];
+        }
+    }
 }
 
 Puzzle::Puzzle(const Puzzle& rhs){
